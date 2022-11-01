6e00dd742e3aaacb7be1400e4c04cbc5f0a86aa4
Bryan-Cardona <bryan.cardona@utp.edu.co>
2022-10-31 22:17:41 -0500

    Update user.model.js

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -4,14 +4,69 @@ const USER_TABLE = 'users';
 
 const UserSchema = {
   dni: {
-    allowNull: false,
-    autoIncrement: false,
-    primaryKey: true,
+    allowNull: true,
     type: DataTypes.INTEGER,
   },
+  tipoUsuario: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    field: 'tipo_usuario',
+  },
   nombre: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  apellidos: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  fechaNacimiento: {
+    allowNull: true,
+    type: DataTypes.DATE,
+    field: 'fecha_nacimiento',
+  },
+  pais: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  departamento: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  ciudad: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  direccion: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  generoFavorito: {
+    allowNull: true,
+    type: DataTypes.STRING,
+    field: 'genero_favorito',
+  },
+  correo: {
+    allowNull: true,
+    type: DataTypes.STRING,
+  },
+  usuario: {
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.STRING,
+  },
+  contrasena: {
     allowNull: false,
     type: DataTypes.STRING,
+  },
+  suscripcionNoticias: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
+    field: 'suscripcion_noticias',
+  },
+  inhabilitar: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
   }
 }
 
