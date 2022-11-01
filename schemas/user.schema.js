68b4c8362f2cb9ed4f503439841633cbed2f5823
Bryan-Cardona <bryan.cardona@utp.edu.co>
2022-11-01 10:04:15 -0500

    Update user.schema.js

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,31 +1,76 @@
 const Joi = require('joi');
 
-const usuario = Joi.string();
-const correo = Joi.string().email();
-const contrasena = Joi.string().min(5);
+const dni = Joi.number().integer().max();
 const tipoUsuario = Joi.number().integer();
+const nombre = Joi.string();
+const apellidos = Joi.string();
+const fechaNacimiento = Joi.date();
+const pais = Joi.string();
+const departamento = Joi.string();
+const ciudad = Joi.string();
+const direccion = Joi.string();
+const generoFavorito = Joi.string();
+const correo = Joi.string().email();
+const usuario = Joi.string();
+const contrasena = Joi.string().min(5);
 const suscripcionNoticias = Joi.boolean();
 const inhabilitar = Joi.boolean();
 
 //const user_type = Joi.number().integer();
 
 const createUserSchema = Joi.object({
+  dni: dni,
+  tipoUsuario: tipoUsuario.required(),
+  nombre: nombre,
+  apellidos: apellidos,
+  fechaNacimiento: fechaNacimiento,
+  pais: pais,
+  departamento: departamento,
+  ciudad: ciudad,
+  direccion: direccion,
+  generoFavorito: generoFavorito,
+  correo: correo,
   usuario: usuario.required(),
   contrasena: contrasena.required(),
-  tipoUsuario: tipoUsuario.required(),
   suscripcionNoticias: suscripcionNoticias.required(),
   inhabilitar: inhabilitar.required()
   //user_type: user_type.required()
 });
 
 const updateUserSchema = Joi.object({
+  dni: dni,
+  tipoUsuario: tipoUsuario,
+  nombre: nombre,
+  apellidos: apellidos,
+  fechaNacimiento: fechaNacimiento,
+  pais: pais,
+  departamento: departamento,
+  ciudad: ciudad,
+  direccion: direccion,
+  generoFavorito: generoFavorito,
+  correo: correo,
   contrasena: contrasena,
-  correo: correo
+  suscripcionNoticias: suscripcionNoticias,
+  inhabilitar: inhabilitar
   //user_type: user_type,
 });
 
 const getUserSchema = Joi.object({
-  usuario: usuario.required(),
+  dni: dni,
+  tipoUsuario: tipoUsuario,
+  nombre: nombre,
+  apellidos: apellidos,
+  fechaNacimiento: fechaNacimiento,
+  pais: pais,
+  departamento: departamento,
+  ciudad: ciudad,
+  direccion: direccion,
+  generoFavorito: generoFavorito,
+  correo: correo,
+  usuario: usuario,
+  contrasena: contrasena,
+  suscripcionNoticias: suscripcionNoticias,
+  inhabilitar: inhabilitar
 });
 
 module.exports = { createUserSchema, updateUserSchema, getUserSchema }
