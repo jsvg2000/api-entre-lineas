542ed450adea4e913b44a2a4faf81a755c6c1f15
unknown <juansebastian.velasquez@utp.edu.co>
2022-11-16 22:09:53 -0500

    Arreglar problemas De Variables Produccion

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,24 +1,22 @@
-const { options } = require('joi');
 const {Pool} = require('pg');
 const {config} = require('./../config/config');
 
-let URI = "";
 
 const options ={}
 
 if(config.isProd){
-  URI = config.dbUrl;
   options.connectionString = config.dbUrl;
   options.ssl = {
     rejectUnauthorized:false
   };
 }else{
-  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  const USER = encodeURIComponent(config.dbUser);
+  const PASSWORD = encodeURIComponent(config.dbPassword);
+  const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  options.connectionString = URI;
 }
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const pool = new Pool();
+const pool = new Pool(options);
 
 
 module.exports = pool;
